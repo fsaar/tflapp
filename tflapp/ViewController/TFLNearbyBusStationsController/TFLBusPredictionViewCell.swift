7cc5f065f5c46e6c3335b491a3e94886f5fbe942
Frank Saar <frank.saar@gmail.com>
2016-11-20 08:47:06 +0000

    revised UI

--- tflapp/ViewController/TFLNearbyBusStationsController/TFLBusPredictionViewCell.swift
+++ tflapp/ViewController/TFLNearbyBusStationsController/TFLBusPredictionViewCell.swift
@@ -7,16 +7,11 @@ class TFLBusPredictionViewCell: UICollectionViewCell {
     private let bgColor = UIColor.init(colorLiteralRed: 0.8, green: 0.8, blue: 0.8, alpha: 1.0)
     override func awakeFromNib() {
         super.awakeFromNib()
+        self.lineBackground.isHidden = true
         self.line.font = UIFont.tflFont(size: 12)
         self.line.textColor = .white
         self.line.isOpaque = true
         self.line.backgroundColor = UIColor.red
-        self.lineBackground.clipsToBounds = true
-        self.lineBackground.layer.cornerRadius = self.lineBackground.frame.size.height/2
-        self.lineBackground.backgroundColor = UIColor.red
-        self.lineBackground.layer.borderColor = UIColor.white.cgColor
-        self.lineBackground.layer.borderWidth = 1
-        self.lineBackground.isOpaque = true
         self.arrivalTime.font = UIFont.tflFont(size: 12)
         self.arrivalTime.textColor = .black
         self.arrivalTime.isOpaque = true
@@ -40,6 +35,13 @@ class TFLBusPredictionViewCell: UICollectionViewCell {
             let path = UIBezierPath(roundedRect:  self.bounds, cornerRadius: 5)
             context.setFillColor(bgColor.cgColor)
             path.fill()
+            
+            let lineBgFrame = self.lineBackground.frame
+            let lineBgPath = UIBezierPath(roundedRect: lineBgFrame , cornerRadius: lineBgFrame.size.height/2)
+            context.setFillColor(UIColor.red.cgColor)
+            context.setStrokeColor(UIColor.white.cgColor)
+            lineBgPath.fill()
+            lineBgPath.stroke()
         }
         
     }

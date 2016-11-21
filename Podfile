9e703cf18e89628443991286abffdc1489bffd12
Frank Saar <frank.saar@gmail.com>
2016-11-21 20:29:24 +0000

    added test target

---
+++ Podfile
@@ -0,0 +1,13 @@
+source 'https://github.com/CocoaPods/Specs.git'
+
+platform :ios, '10.0'
+inhibit_all_warnings!
+
+target 'tflapp' do
+end
+
+target 'tflApp Tests' do
+    use_frameworks!
+    pod 'Quick', '0.10.0' 
+    pod 'Nimble', '5.1.1'
+end
---
+++ tflApp Tests/TFLBusPredicationSpecs.swift
@@ -0,0 +1,18 @@
+
+import Quick
+import Nimble
+
+class TFLBusPredicationSpecs: QuickSpec {
+    
+    override func spec() {
+        
+        
+        beforeEach() {
+            
+        }
+        
+        it ("should do something") {
+            expect(2+2) == 4
+        }
+    }
+}

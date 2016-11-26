314dd777751f2b7e13e64181abfb311c1e408e69
Frank Saar <frank.saar@gmail.com>
2016-11-26 13:42:42 +0000

    revised : perform insert, delete & move operations in one update block

--- tflapp/Protocols/TFLTransitionable.swift
+++ tflapp/Protocols/TFLTransitionable.swift
@@ -17,26 +17,14 @@ extension UITableView : Transitionable {
         self.insertRows(at: insertedIndexPaths , with: .automatic)
         let deledtedIndexPaths = deleted.map { IndexPath(row: $0.index,section:0)}
         self.deleteRows(at: deledtedIndexPaths , with: .automatic)
+        moved.forEach { self.moveRow(at: IndexPath(row: $0.oldIndex,section:0), to:  IndexPath(row: $0.newIndex,section:0)) }
         self.endUpdates()
         
-        if !updated.isEmpty {
-            self.beginUpdates()
-            let updatedIndexPaths = updated.map { IndexPath(row: $0.index,section:0)}
-            updateBlock(updatedIndexPaths)
-            self.endUpdates()
-        }
-        
-        if !moved.isEmpty {
-            self.beginUpdates()
-            moved.forEach { self.moveRow(at: IndexPath(row: $0.oldIndex,section:0), to:  IndexPath(row: $0.newIndex,section:0)) }
-            self.endUpdates()
-            
-            self.beginUpdates()
-            let updatedIndexPaths = moved.map { IndexPath(row: $0.newIndex,section:0)}
-            updateBlock(updatedIndexPaths)
-            self.endUpdates()
-        }
-        
+        self.beginUpdates()
+        let updatedIndexPaths = updated.map { IndexPath(row: $0.index,section:0)}
+        let movedIndexPaths = moved.map { IndexPath(row: $0.newIndex,section:0)}
+        updateBlock(updatedIndexPaths+movedIndexPaths)
+        self.endUpdates()
     }
     
 }

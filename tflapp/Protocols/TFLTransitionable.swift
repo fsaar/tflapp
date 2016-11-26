23c5e097e1c1034e9bd7b364e118d24c40ecf162
Frank Saar <frank.saar@gmail.com>
2016-11-26 15:51:11 +0000

    revised order of update operations

--- tflapp/Protocols/TFLTransitionable.swift
+++ tflapp/Protocols/TFLTransitionable.swift
@@ -15,9 +15,9 @@ extension UITableView : Transitionable {
         self.beginUpdates()
         let insertedIndexPaths = inserted.map { IndexPath(row: $0.index,section:0)}
         self.insertRows(at: insertedIndexPaths , with: .automatic)
+        moved.forEach { self.moveRow(at: IndexPath(row: $0.oldIndex,section:0), to:  IndexPath(row: $0.newIndex,section:0)) }
         let deledtedIndexPaths = deleted.map { IndexPath(row: $0.index,section:0)}
         self.deleteRows(at: deledtedIndexPaths , with: .automatic)
-        moved.forEach { self.moveRow(at: IndexPath(row: $0.oldIndex,section:0), to:  IndexPath(row: $0.newIndex,section:0)) }
         self.endUpdates()
         
         self.beginUpdates()

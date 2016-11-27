9c7d1424f9301af073b30e5f13a9704b67072bbc
Frank Saar <frank.saar@gmail.com>
2016-11-27 14:15:26 +0000

    added transitional collection view support

--- tflapp/Protocols/TFLTransitionable.swift
+++ tflapp/Protocols/TFLTransitionable.swift
@@ -3,12 +3,12 @@ import UIKit
 import Foundation
 
 protocol Transitionable : TFLChangeSetProtocol {
-    func transition<T : Equatable & Hashable>(from oldArrivalInfo: [T], to newArrivalInfo: [T],with compare: @escaping (_ lhs : T,_ rhs: T) -> (Bool), using updateBlock: ([IndexPath]) -> ())
+    func transition<T : Equatable & Hashable>(from oldArrivalInfo: [T], to newArrivalInfo: [T],with compare: @escaping (_ lhs : T,_ rhs: T) -> (Bool), using updateBlock:@escaping ([IndexPath]) -> ())
 }
 
 
 extension UITableView : Transitionable {
-    func transition<T : Equatable & Hashable>(from oldArrivalInfo: [T], to newArrivalInfo: [T],with compare: @escaping (_ lhs : T,_ rhs: T) -> (Bool), using updateBlock: ([IndexPath]) -> ()) {
+    func transition<T : Equatable & Hashable>(from oldArrivalInfo: [T], to newArrivalInfo: [T],with compare: @escaping (_ lhs : T,_ rhs: T) -> (Bool), using updateBlock:@escaping ([IndexPath]) -> ()) {
         
         let (inserted ,deleted ,updated, moved) = self.evaluateLists(oldList: oldArrivalInfo, newList: newArrivalInfo, compare : compare)
         
@@ -25,6 +25,28 @@ extension UITableView : Transitionable {
         let movedIndexPaths = moved.map { IndexPath(row: $0.newIndex,section:0)}
         updateBlock(updatedIndexPaths+movedIndexPaths)
         self.endUpdates()
+    }
+    
+}
+
+
+extension UICollectionView : Transitionable {
+    func transition<T : Equatable & Hashable>(from oldArrivalInfo: [T], to newArrivalInfo: [T],with compare: @escaping (_ lhs : T,_ rhs: T) -> (Bool), using updateBlock: @escaping ([IndexPath]) -> ()) {
+        
+        let (inserted ,deleted ,updated, moved) = self.evaluateLists(oldList: oldArrivalInfo, newList: newArrivalInfo, compare : compare)
+        
+        self.performBatchUpdates({
+            let insertedIndexPaths = inserted.map { IndexPath(item: $0.index,section:0)}
+            self.insertItems(at: insertedIndexPaths )
+            moved.forEach { self.moveItem(at: IndexPath(item: $0.oldIndex,section:0), to:  IndexPath(item: $0.newIndex,section:0)) }
+            let deledtedIndexPaths = deleted.map { IndexPath(item: $0.index,section:0)}
+            self.deleteItems(at: deledtedIndexPaths)
+            
+        }) { _ in
+            let updatedIndexPaths = updated.map { IndexPath(item: $0.index,section:0)}
+            let movedIndexPaths = moved.map { IndexPath(item: $0.newIndex,section:0)}
+            updateBlock(updatedIndexPaths+movedIndexPaths)
+        }
     }
     
 }

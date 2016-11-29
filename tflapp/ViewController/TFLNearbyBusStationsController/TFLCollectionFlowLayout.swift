7d3c80d69b76dbf07fced863841a162f741ea96f
Frank Saar <frank.saar@gmail.com>
2016-11-29 08:42:27 +0000

    revised insert animation

--- tflapp/ViewController/TFLNearbyBusStationsController/TFLCollectionFlowLayout.swift
+++ tflapp/ViewController/TFLNearbyBusStationsController/TFLCollectionFlowLayout.swift
@@ -1,10 +1,15 @@
 import UIKit
 
+enum TFLCollectionFlowLayoutAppearingItemAnimationType {
+    case scale
+    case translate
+}
+
 class TFLCollectionFlowLayout : UICollectionViewFlowLayout {
     
     var deleteIndexPaths : [IndexPath] = []
     var insertIndexPaths : [IndexPath] = []
-    lazy var finalTransform : CGAffineTransform = CGAffineTransform.identity.scaledBy(x: 0.01, y: 0.01)
+    lazy var scaleTransform : CGAffineTransform = CGAffineTransform.identity.scaledBy(x: 0.1, y: 0.1)
     
     override func finalizeCollectionViewUpdates() {
         self.deleteIndexPaths = []
@@ -12,8 +17,9 @@ class TFLCollectionFlowLayout : UICollectionViewFlowLayout {
     }
     override func prepare(forCollectionViewUpdates updateItems: [UICollectionViewUpdateItem]) {
         super.prepare(forCollectionViewUpdates: updateItems)
-        self.deleteIndexPaths = updateItems.filter { $0.updateAction == .delete }.flatMap { $0.indexPathBeforeUpdate }
-        self.insertIndexPaths = updateItems.filter { $0.updateAction == .insert }.flatMap { $0.indexPathAfterUpdate }
+        self.deleteIndexPaths = updateItems.filter { $0.updateAction == .delete }.flatMap { $0.indexPathBeforeUpdate }.sorted { $0.item < $1.item }
+        self.insertIndexPaths = updateItems.filter { $0.updateAction == .insert }.flatMap { $0.indexPathAfterUpdate }.sorted { $1.item < $1.item }
+
     }
     
     
@@ -21,7 +27,13 @@ class TFLCollectionFlowLayout : UICollectionViewFlowLayout {
         let layoutAttributes = super.initialLayoutAttributesForAppearingItem(at: itemIndexPath)
         
         if self.insertIndexPaths.contains(itemIndexPath), let width = self.collectionView?.frame.size.width {
-            layoutAttributes?.frame.origin.x += width
+            let animationType = appearingAnimationType(forItemAppearingAt: itemIndexPath)
+            switch animationType {
+            case .translate:
+                layoutAttributes?.frame.origin.x += width
+            case .scale:
+                layoutAttributes?.transform = self.scaleTransform
+            }
         }
         return layoutAttributes
     }
@@ -29,9 +41,22 @@ class TFLCollectionFlowLayout : UICollectionViewFlowLayout {
     override func finalLayoutAttributesForDisappearingItem(at itemIndexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
         let layoutAttributes = super.finalLayoutAttributesForDisappearingItem(at: itemIndexPath)
         if self.deleteIndexPaths.contains(itemIndexPath) {
-            layoutAttributes?.transform = self.finalTransform
+            layoutAttributes?.transform = self.scaleTransform
         }
         return layoutAttributes
     }
 
+}
+
+// MARK: Private
+
+private extension TFLCollectionFlowLayout {
+    func appearingAnimationType(forItemAppearingAt indexPath: IndexPath) -> (TFLCollectionFlowLayoutAppearingItemAnimationType) {
+        var animationType = TFLCollectionFlowLayoutAppearingItemAnimationType.translate
+        if let startIndex = self.insertIndexPaths.index(of: indexPath),let itemCount = self.collectionView?.numberOfItems(inSection: 0), startIndex+1 < self.insertIndexPaths.count  {
+            let succeedingItemCount = self.insertIndexPaths[startIndex+1..<self.insertIndexPaths.count].count
+            animationType = ((itemCount-1) - indexPath.row) == succeedingItemCount ? TFLCollectionFlowLayoutAppearingItemAnimationType.translate : TFLCollectionFlowLayoutAppearingItemAnimationType.scale
+        }
+        return animationType
+    }
 }

c59e585b8d894f01dc443c04dd1e79d7bce20e80
Frank Saar <frank.saar@gmail.com>
2016-12-17 20:08:14 +0000

    revised slide handling

--- tflapp/ViewController/TFLSlideContainerController/TFLSlideContainerController.swift
+++ tflapp/ViewController/TFLSlideContainerController/TFLSlideContainerController.swift
@@ -1,17 +1,11 @@
 
 import UIKit
 
-enum TFLSlideContainerControllerState {
-    case bottom
-    case middle
-    case top
-    
-}
-
 class TFLSlideContainerController: UIViewController {
-    var state : TFLSlideContainerControllerState?
-    lazy var slideOffset : (top:CGFloat,bottom:CGFloat) = (10,self.effectsViewContainerView.frame.size.height+125)
-    private var yOffset : CGFloat = 0
+    var snapPositions: [CGFloat] = [0.01,0.4,0.75]
+    fileprivate var snapHandler : TFLSnapHandler?
+    var sliderViewUpdateBlock : ((_ slider : UIView,_ origin: CGPoint,_ final : Bool) -> ())?
+    
     fileprivate lazy var shapeLayer : CAShapeLayer = {
         let path = CGMutablePath()
         let radius = CGFloat(20)
@@ -19,15 +13,15 @@ class TFLSlideContainerController: UIViewController {
         path.addLine(to: CGPoint(x:self.view.frame.size.width-radius,y:0))
         
         path.addArc(center: CGPoint(x:self.view.frame.size.width-radius,y:radius), radius: radius, startAngle: CGFloat(1.5 * M_PI), endAngle: 0, clockwise: false)
-        path.addLine(to: CGPoint(x:self.view.frame.size.width,y:self.effectsViewContainerView.frame.size.height))
-        path.addLine(to: CGPoint(x:0,y:self.effectsViewContainerView.frame.size.height))
+        path.addLine(to: CGPoint(x:self.view.frame.size.width,y:self.sliderHandleContainerView.frame.size.height))
+        path.addLine(to: CGPoint(x:0,y:self.sliderHandleContainerView.frame.size.height))
         path.addLine(to: CGPoint(x:0,y:radius))
         path.addArc(center: CGPoint(x:radius,y:radius), radius: radius, startAngle: CGFloat(M_PI) , endAngle:  CGFloat(1.5 * M_PI), clockwise: false)
         let shapeLayer = CAShapeLayer()
         shapeLayer.path = path
         return shapeLayer
     }()
-    @IBOutlet fileprivate weak var effectsViewContainerView : UIView!
+    @IBOutlet fileprivate weak var sliderHandleContainerView : UIView!
     @IBOutlet fileprivate weak var backgroundContainerView : UIView!
     @IBOutlet fileprivate var sliderContainerView : UIView! = nil
     @IBOutlet fileprivate var sliderContentContainerView : UIView!
@@ -37,81 +31,34 @@ class TFLSlideContainerController: UIViewController {
             self.sliderHandleView.layer.cornerRadius = self.sliderHandleView.frame.size.height/2
         }
     }
+  
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.effectsViewContainerView.layer.mask = self.shapeLayer
+        self.sliderHandleContainerView.layer.mask = self.shapeLayer
+        self.snapHandler = TFLSnapHandler(with: self.sliderHandleContainerView,in: self.view, and: self.snapPositions, using: { [weak self] view,newOrigin,final in
+            if let strongSelf = self {
+                strongSelf.sliderContainerViewTopConstraint.constant = newOrigin.y
+                if (final) {
+                    UIView.animate(withDuration: 0.25, delay: 0, options: .curveEaseOut, animations: {
+                        strongSelf.view.layoutIfNeeded()
+                    }, completion: nil)
+                }
+                else
+                {
+                    strongSelf.view.layoutIfNeeded()
+                }
+                strongSelf.sliderViewUpdateBlock?(view,newOrigin,final)
+            }
+        })
+        self.sliderContainerViewTopConstraint.constant = (self.snapPositions.first ?? 0) * self.view.frame.size.height
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        self.updateState(to: state ?? .top)
-    }
     
     func setContentControllers(with backgroundController : UIViewController,and sliderController : UIViewController) {
         add(backgroundController,to: backgroundContainerView )
         add(sliderController,to: sliderContentContainerView )
     }
-    
-    @IBAction func gestureHandler(_ recognizer : UIGestureRecognizer) {
-        let p = recognizer.location(in: self.view)
-        switch recognizer.state {
-        case .began:
-            self.yOffset = recognizer.location(in: self.effectsViewContainerView).y
-        case .changed:
-            let normalisedOrigin =  p.y - yOffset
-            let yOrigin = max(min(normalisedOrigin,self.view.frame.size.height - self.slideOffset.bottom),self.slideOffset.top)
-            self.sliderContainerViewTopConstraint.constant = yOrigin
-            self.view.layoutIfNeeded()
-        case .ended:
-            let newState : TFLSlideContainerControllerState = self.state(for: self.sliderContainerViewTopConstraint.constant)
-            self.updateState(to: newState, animated: true)
-        default:
-            break
-        }
-    }
-}
-
-/// MARK: State Handling
-
-fileprivate extension TFLSlideContainerController {
-    func updateState(to state: TFLSlideContainerControllerState, animated : Bool = false) {
-        self.state = state
-        self.sliderContainerViewTopConstraint.constant = topOffset(for: state)
-        let duration = animated ? 0.25 : 0.0
-        UIView.animate(withDuration: duration, delay: 0, options: .curveEaseOut, animations: {
-            self.view.layoutIfNeeded()
-        }, completion: nil)
-    }
-    
-    func topOffset(for state: TFLSlideContainerControllerState) -> CGFloat {
-        var offset : CGFloat = 0
-        switch state {
-        case .bottom:
-            offset =  self.view.frame.size.height - self.slideOffset.bottom
-        case .top:
-            offset =  self.slideOffset.top
-        case .middle:
-            offset =  self.view.frame.size.height/3
-        }
-        return offset
-    }
-    
-    
-    func state(for topOffset: CGFloat) -> TFLSlideContainerControllerState {
-        var state : TFLSlideContainerControllerState = .middle
-        let height = self.view.frame.size.height
-        switch topOffset {
-        case 0..<height/4:
-            state = .top
-        case height/4..<(5*height/8):
-            state = .middle
-        default:
-            state = .bottom
-        }
-        return state
-    }
-
 }
 
 fileprivate extension TFLSlideContainerController {
---
+++ tflapp/ViewController/TFLSlideContainerController/TFLSnapHandler.swift
@@ -0,0 +1,52 @@
+import UIKit
+
+typealias TFLSnapHandlerUpdateBlock = (_ pangestureView : UIView,_ newOrigin : CGPoint, _ final : Bool) -> ()
+
+class TFLSnapHandler: NSObject {
+    var panGestureView : UIView
+    var panGestureStartY : CGFloat = 0
+    let snapPositions : [CGFloat]
+    var gestureRecognizer : UIPanGestureRecognizer?
+    var snapHandlerUpdateBlock : TFLSnapHandlerUpdateBlock
+    var containerView : UIView
+    init(with panGestureView: UIView,in containerView: UIView, and snapPositions: [CGFloat], using snapHandlerUpdateBlock: @escaping TFLSnapHandlerUpdateBlock ) {
+        self.panGestureView = panGestureView
+        self.containerView = containerView
+        self.snapHandlerUpdateBlock = snapHandlerUpdateBlock
+        self.snapPositions  = snapPositions
+        super.init()
+        self.gestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(panGestureHandler(_:)))
+        panGestureView.addGestureRecognizer(self.gestureRecognizer!)
+    }
+    
+    func panGestureHandler(_ recognizer : UIGestureRecognizer) {
+        let p = recognizer.location(in: self.containerView)
+        let pGestureView = recognizer.location(in: self.panGestureView)
+        switch recognizer.state {
+        case .began:
+            self.panGestureStartY = pGestureView.y
+        case .changed:
+            let origin = CGPoint(x:panGestureView.frame.origin.x,y:p.y - self.panGestureStartY )
+            self.snapHandlerUpdateBlock(self.panGestureView, origin, false)
+        case .ended:
+            let originY = closestSnapPositionY(with: containerView,for: p.y - self.panGestureStartY )
+            let origin = CGPoint(x:panGestureView.frame.origin.x,y:originY ?? 0)
+            self.snapHandlerUpdateBlock(self.panGestureView, origin, true)
+        default:
+            break
+        }
+    }
+}
+
+// MARK: Private
+
+extension TFLSnapHandler {
+    fileprivate func closestSnapPositionY(with view: UIView, for positionY : CGFloat) -> CGFloat? {
+        let translatedSnapPositions = self.snapPositions.map { view.frame.size.height * $0 }
+
+        let distances = translatedSnapPositions.map { ($0,fabs($0 - positionY)) }
+        let sortedDistance = distances.sorted (by: { $0.1 < $1.1 })
+        let positionY = sortedDistance.first?.0
+        return positionY
+    }
+}

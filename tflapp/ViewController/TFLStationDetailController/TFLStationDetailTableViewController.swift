2d48e2bf817b5cbe500d558c7b31f52c0d837d10
Frank Saar <frank.saar@gmail.com>
2018-06-02 11:31:50 +0100

    revamped collapsible section header

--- tflapp/ViewController/TFLStationDetailController/TFLStationDetailTableViewController.swift
+++ tflapp/ViewController/TFLStationDetailController/TFLStationDetailTableViewController.swift
@@ -10,33 +10,16 @@ import UIKit
 import CoreData
 
 class TFLStationDetailTableViewController: UITableViewController {
-    enum SectionState {
-        case open(model : TFLStationDetailTableViewModel)
-        case closed(model : TFLStationDetailTableViewModel)
-        
-        var inverse : SectionState {
-            switch self {
-            case .open(let model):
-                return .closed(model: model)
-                
-            case .closed(let model):
-                return .open(model: model)
-            }
-        }
-    }
     
     let sectionHeaderDefaultHeight = CGFloat(50)
-    var tableViewModel : [SectionState] = [] {
+   
+    var viewModels : [TFLStationDetailTableViewModel] = [] {
         didSet {
             self.tableView.reloadData()
         }
-
     }
-    var viewModels : [TFLStationDetailTableViewModel] = [] {
-        didSet {
-            self.tableViewModel = self.viewModels.map {SectionState.closed(model: $0) }
-        }
-    }
+    
+   
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -55,21 +38,14 @@ extension TFLStationDetailTableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        let model = tableViewModel[section]
-        switch model {
-        case .open(let viewModel):
-            return viewModel.stations.count
-        case .closed:
-            return 0
-        }
+        let model = viewModels[section]
+        return model.stations.count
     }
     
     override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         let header = tableView.dequeueReusableHeaderFooterView(withIdentifier: String(describing: TFLStationDetailSectionHeaderView.self)) as? TFLStationDetailSectionHeaderView
          let model = viewModels[section]
-        header?.configure(with: model) { [weak self] in
-            self?.didTap(section: section)
-        }
+        header?.configure(with: model)
         return header
     }
     
@@ -77,28 +53,10 @@ extension TFLStationDetailTableViewController {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: TFLStationDetailTableViewCell.self), for: indexPath)
         if let cell = cell as? TFLStationDetailTableViewCell {
-            let model = tableViewModel[indexPath.section]
-            if case let .open(viewModel) = model {
-                cell.configure(with: viewModel,at:indexPath.row)
-            }
+            let model = viewModels[indexPath.section]
+            cell.configure(with: model,at:indexPath.row)
         }
         return cell
     }
 }
-// MARK: Private
-private extension TFLStationDetailTableViewController {
-    func didTap(section : Int) {
-        let model = tableViewModel[section]
-        tableView.beginUpdates()
-        tableViewModel[section] = model.inverse
-        switch tableViewModel[section] {
-        case .closed(let viewModel):
-            let indexPaths = (0..<viewModel.stations.count).map { IndexPath(row: $0, section:section ) }
-            tableView.deleteRows(at: indexPaths, with: .fade)
-        case .open(let viewModel):
-            let indexPaths = (0..<viewModel.stations.count).map { IndexPath(row: $0, section:section ) }
-            tableView.insertRows(at: indexPaths, with: .fade)
-        }
-        tableView.endUpdates()
-    }
-}
+

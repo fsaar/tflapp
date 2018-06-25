f0f72481886dadad0c225c21563b1fbdb6ff9d42
Frank Saar <frank.saar@gmail.com>
2018-06-25 19:28:17 +0100

    revised tests

--- tflApp Tests/TFLChangeSetProtocolSpecs.swift
+++ tflApp Tests/TFLChangeSetProtocolSpecs.swift
@@ -174,14 +174,32 @@ class TFLChangeSetProtocolSpecs : QuickSpec {
             expect(Set(moved.map { MovedPos($0) })) == Set([MovedPos(M("7",1),3,1)])
         }
         
-        fit("should return the correct tuple when moved 5 to the end plus 2 new inserts: ([3,5] -> [3,1,2,5]") {
+        it("should return the correct tuple when moved 5 to the end plus 2 new inserts: ([1,5] -> [3,1,2,5]") {
             let oldList = [1,5].map { M("\($0)",$0) }
             let newList =  [M("1",1),M("2",2),M("3",3),M("5",5)]
             let (inserted ,deleted ,updated, moved)  = changeSetHandler.evaluateLists(oldList: oldList, newList: newList, sortedBy : M.compare)
             expect(Set(inserted.map { Pos($0) })) == Set([Pos(M("2",2),1),Pos(M("3",3),2)])
             expect(Set(deleted.map { Pos($0) })) == Set([])
-            expect(Set(updated.map { Pos($0) })) == Set([])
-            expect(Set(moved.map { MovedPos($0) })) == Set([MovedPos(M("5",5),0,2)])
+            expect(Set(updated.map { Pos($0) })) == Set([Pos(M("1",1),0),Pos(M("5",5),3)])
+            expect(Set(moved.map { MovedPos($0) })) == Set([])
+        }
+        it("should return the correct tuple when moving 1 to next pos and inserting 2: ([1,3,5] -> [3,1,2,5]") {
+            let oldList = [1,3,5].map { M("\($0)",$0) }
+            let newList =  [M("3",0),M("1",1),M("2",2),M("5",5)]
+            let (inserted ,deleted ,updated, moved)  = changeSetHandler.evaluateLists(oldList: oldList, newList: newList, sortedBy : M.compare)
+            expect(Set(inserted.map { Pos($0) })) == Set([Pos(M("2",2),2)])
+            expect(Set(deleted.map { Pos($0) })) == Set([])
+            expect(Set(updated.map { Pos($0) })) == Set([Pos(M("3",0),0),Pos(M("5",5),3)])
+            expect(Set(moved.map { MovedPos($0) })) == Set([MovedPos(M("1",1),0,1)])
+        }
+        it("should return the correct tuple when deleting 1 to next pos and inserting 2: ([1,3,5] -> [3,2,5]") {
+            let oldList = [1,3,5].map { M("\($0)",$0) }
+            let newList =  [M("3",0),M("2",2),M("5",5)]
+            let (inserted ,deleted ,updated, moved)  = changeSetHandler.evaluateLists(oldList: oldList, newList: newList, sortedBy : M.compare)
+            expect(Set(inserted.map { Pos($0) })) == Set([Pos(M("2",2),1)])
+            expect(Set(deleted.map { Pos($0) })) == Set([Pos(M("1",1),0)])
+            expect(Set(updated.map { Pos($0) })) == Set([Pos(M("3",0),0),Pos(M("5",5),2)])
+            expect(Set(moved.map { MovedPos($0) })) == Set([])
         }
 
     }

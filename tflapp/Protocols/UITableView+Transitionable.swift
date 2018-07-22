d5a47b87ee32895ac37da3dcddbc4e09a0a37ba4
Frank Saar <frank.saar@gmail.com>
2018-07-22 18:20:03 +0100

    delete entries top to bottom, iinsert entries bottom to top

--- tflapp/Protocols/UITableView+Transitionable.swift
+++ tflapp/Protocols/UITableView+Transitionable.swift
@@ -4,7 +4,11 @@ import Foundation
 protocol Transitionable  {
     func transition<T : Hashable>(from oldArrivalInfo: [T], to newArrivalInfo: [T],with compare: @escaping (_ lhs : T,_ rhs: T) -> (Bool), using updateBlock: @escaping  ([IndexPath]) -> ())
 }
-
+extension Collection where Element == Int  {
+    func indexPaths(_ section : Int = 0) -> [IndexPath] {
+        return self.map { IndexPath(item: $0, section: section) }
+    }
+}
 
 extension UITableView : Transitionable {
     func transition<T : Hashable>(from oldArrivalInfo: [T], to newArrivalInfo: [T],with compare: @escaping (_ lhs : T,_ rhs: T) -> (Bool), using updateBlock: @escaping  ([IndexPath]) -> ()) {
@@ -13,14 +17,14 @@ extension UITableView : Transitionable {
             let (inserted ,deleted ,updated, moved) = oldArrivalInfo.transformTo(newList: newArrivalInfo, sortedBy : compare)
             DispatchQueue.main.async {
                 self.performBatchUpdates({
-                    let deletedIndexPaths = deleted.map { IndexPath(row: $0.index,section:0) }
+                    let deletedIndexPaths = deleted.map ({ $0.index }).indexPaths().sorted(by:>)
                     self.deleteRows(at: deletedIndexPaths , with: .automatic)
-                    let insertedIndexPaths = inserted.map { IndexPath(row: $0.index,section:0) }
+                    let insertedIndexPaths = inserted.map ({ $0.index }).indexPaths().sorted(by:<)
                     self.insertRows(at: insertedIndexPaths , with: .automatic)
                     moved.forEach { self.moveRow(at: IndexPath(row: $0.oldIndex,section:0), to:  IndexPath(row: $0.newIndex,section:0)) }
                 }, completion: { _ in
-                    let updatedIndexPaths = updated.map { IndexPath(row: $0.index,section:0) }
-                    let movedIndexPaths = moved.map { IndexPath(row: $0.newIndex,section:0) }
+                    let updatedIndexPaths = updated.map ({ $0.index}).indexPaths()
+                    let movedIndexPaths = moved.map ({ $0.newIndex }).indexPaths()
                     updateBlock(updatedIndexPaths+movedIndexPaths)
                 })
             }

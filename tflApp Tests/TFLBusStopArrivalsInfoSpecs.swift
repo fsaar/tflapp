3286ab6a3d01afb157b6a79a0b995c2511467e34
Frank Saar <frank.saar@gmail.com>
2018-07-24 23:26:17 +0100

    added encoding / decoding tests

--- tflApp Tests/TFLBusStopArrivalsInfoSpecs.swift
+++ tflApp Tests/TFLBusStopArrivalsInfoSpecs.swift
@@ -59,8 +59,70 @@ class TFLBusStopArrivalsInfoSpecs: QuickSpec {
         
         context("when dealing with TFLBusStopArrivalsInfo") {
             describe("and codable protocol") {
-                pending ("should encode TFLBusStopArrivalsInfo correctly") {
+                var busPrediction1 : TFLBusPrediction!
+                var busPrediction2 : TFLBusPrediction!
+                var coord : CLLocationCoordinate2D!
+                
+                var busStationInfo : TFLBusStopArrivalsInfo.TFLContextFreeBusStopInfo!
+                var location :  CLLocation!
+                var info : TFLBusStopArrivalsInfo!
+                beforeEach() {
+                    busPrediction1 = TFLBusPrediction(identifier: "identifier1", timeToLive: Date(timeIntervalSinceNow: 50), timeStamp: Date(), busStopIdentifier: "busstop1", lineIdentifier: "line1", lineName: "lineName", destination: "destination", timeToStation: 40)
+                    busPrediction2 = TFLBusPrediction(identifier: "identifier2", timeToLive: Date(timeIntervalSinceNow: 50), timeStamp: Date(), busStopIdentifier: "busstop2", lineIdentifier: "line2", lineName: "lineName2", destination: "destination2", timeToStation: 80)
+                    coord = CLLocationCoordinate2D(latitude: 0.5, longitude: 0.5)
                     
+                    busStationInfo = TFLBusStopArrivalsInfo.TFLContextFreeBusStopInfo(identifier: "id", stopLetter: "ST", towards: "destination", name: "name", coord: coord)
+                    location =  CLLocation(latitude: coord.latitude, longitude: coord.longitude)
+                    info = TFLBusStopArrivalsInfo(busStop:busStationInfo , location: location, arrivals: [busPrediction1,busPrediction2])
+                }
+                
+                fit ("should encode TFLBusStopArrivalsInfo correctly") {
+                   
+                    let data = try! JSONEncoder().encode(info)
+                    expect(data).notTo(beNil())
+                }
+                
+                fit ("should decode TFLBusStopArrivalsInfo correctly") {
+                    
+                    let data = try! JSONEncoder().encode(info)
+                    let info2 = try! JSONDecoder().decode(TFLBusStopArrivalsInfo.self, from: data)
+                    expect(info2).notTo(beNil())
+                    let pred1 = info2.arrivals[0]
+                    let pred2 = info2.arrivals[1]
+                    expect(pred1.identifier) == busPrediction1.identifier
+                    expect(pred2.identifier) == busPrediction2.identifier
+                    
+                    expect(pred1.timeToLive) == busPrediction1.timeToLive
+                    expect(pred2.timeToLive) == busPrediction2.timeToLive
+                    
+                    expect(pred1.timeStamp) == busPrediction1.timeStamp
+                    expect(pred2.timeStamp) == busPrediction2.timeStamp
+                    
+                    expect(pred1.busStopIdentifier) == busPrediction1.busStopIdentifier
+                    expect(pred2.busStopIdentifier) == busPrediction2.busStopIdentifier
+                    
+                    expect(pred1.lineIdentifier) == busPrediction1.lineIdentifier
+                    expect(pred2.lineIdentifier) == busPrediction2.lineIdentifier
+                    
+                    expect(pred1.lineName) == busPrediction1.lineName
+                    expect(pred2.lineName) == busPrediction2.lineName
+                    
+                    expect(pred1.destination) == busPrediction1.destination
+                    expect(pred2.destination) == busPrediction2.destination
+                    
+                    expect(pred1.timeToStation) == busPrediction1.timeToStation
+                    expect(pred2.timeToStation) == busPrediction2.timeToStation
+                    
+                    expect(info2.busStop.coord.latitude) == info.busStop.coord.latitude
+                    expect(info2.busStop.coord.longitude) == info.busStop.coord.longitude
+                    expect(info2.busStop.identifier) == info.busStop.identifier
+                    expect(info2.busStop.stopLetter) == info.busStop.stopLetter
+                    expect(info2.busStop.towards) == info.busStop.towards
+                    expect(info2.busStop.name) == info.busStop.name
+                    
+                    expect(info2.busStopDistance) == info.busStopDistance
+                   
+
                 }
                
             }

8e671d897658d235d20fd5bb6a47ac98a5f47597
Frank Saar <frank.saar@gmail.com>
2018-10-26 22:21:46 +0100

    added test coverage for TFLHUD

---
+++ tflApp Tests/TFLHUDSpecs.swift
@@ -0,0 +1,35 @@
+import Quick
+import Nimble
+import CoreLocation
+import UIKit
+
+@testable import London_Bus
+class TFLHUDSpecs: QuickSpec {
+    
+    override func spec() {
+        
+        context("when showing TFLHUD") {
+            it("should add visualeffectsView to window") {
+                TFLHUD.show()
+                let delegate  = UIApplication.shared.delegate as! AppDelegate
+                let window  = delegate.window
+                let views = window?.subviews.filter { $0 is UIVisualEffectView }
+                expect(views!.count) == 1
+            }
+        }
+        
+        context("when hiding TFLHUD") {
+            it("should remove visualeffectsView to window") {
+                TFLHUD.show()
+                TFLHUD.hide()
+                let delegate  = UIApplication.shared.delegate as! AppDelegate
+                let window  = delegate.window
+                let views = window?.subviews.filter { $0 is UIVisualEffectView }
+                expect(views!.count) == 0
+            }
+        }
+        
+    }
+            
+}
+

97dae135ca147060f472b360cc4d0aba1bf46460
Frank Saar <frank.saar@gmail.com>
2018-11-04 07:35:52 +0000

    revised helper method to support text in image

--- tflapp/Common/Extensions/UIImage+Helper.swift
+++ tflapp/Common/Extensions/UIImage+Helper.swift
@@ -11,28 +11,40 @@ import MapKit
 
 extension UIImage {
 
-    static func imageForPos(_ pos : CLLocationCoordinate2D,using completionBlock: @escaping (_ image : UIImage?) -> Void)  {
+    static func imageForPos(_ pos : CLLocationCoordinate2D,_ text : String? = nil,using completionBlock: @escaping (_ image : UIImage?) -> Void)  {
+        
         let mapSnapshotOptions = MKMapSnapshotter.Options()
         
         let location = CLLocationCoordinate2DMake(pos.latitude, pos.longitude)
-        let region = MKCoordinateRegion(center: location, latitudinalMeters: 100, longitudinalMeters: 100)
+        let region = MKCoordinateRegion(center: location, latitudinalMeters: 300, longitudinalMeters: 300)
         mapSnapshotOptions.region = region
-        
-        // Set the scale of the image. We'll just use the scale of the current device, which is 2x scale on Retina screens.
         mapSnapshotOptions.scale = UIScreen.main.scale
-        
-        // Set the size of the image output.
         mapSnapshotOptions.size = CGSize(width:300,height: 300)
-        
-        // Show buildings and Points of Interest on the snapshot
         mapSnapshotOptions.showsBuildings = true
         mapSnapshotOptions.showsPointsOfInterest = true
-        
+ 
         let snapShotter = MKMapSnapshotter(options: mapSnapshotOptions)
         snapShotter.start { snapshot,_ in
-            let image = snapshot?.image
-            completionBlock(image)
+            guard let image = snapshot?.image else {
+                completionBlock(nil)
+                return
+            }
+            let render = UIGraphicsImageRenderer(bounds: CGRect(origin: .zero, size: image.size) ,format: UIGraphicsImageRendererFormat())
+            let newImage = render.image { _ in
+                image.draw(at: .zero)
+                
+                let bounds = CGRect(origin: CGPoint(x:image.size.width / 2,y:image.size.height / 2 ), size: CGSize(width: 10, height: 10))
+                let borderPath = UIBezierPath(roundedRect: bounds , cornerRadius: bounds.size.height/2)
+                UIColor.red.setFill()
+                borderPath.fill()
+                if let text = text {
+                    let attributedString = NSAttributedString(string: text)
+                    attributedString.draw(at: CGPoint(x:10,y:image.size.height-20))
+                }
+            }
+            completionBlock(newImage)
         }
+    
     }
 
 }

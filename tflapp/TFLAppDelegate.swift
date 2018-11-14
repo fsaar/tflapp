331e0928e781efa23cfb5da824a947ed604bef0d
Frank Saar <frank.saar@gmail.com>
2018-11-14 22:37:33 +0000

    feature/corespotlight (#29)
    * added initial spotlight db support
    
    * added spotlight support

--- tflapp/TFLAppDelegate.swift
+++ tflapp/TFLAppDelegate.swift
@@ -1,15 +1,25 @@
 import UIKit
 import CoreData
+import CoreSpotlight
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+ 
     var window: UIWindow?
     func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         initCoreData()
+        setupSpotLight()
         _ = TFLLocationManager.sharedManager
         return true
-
+    }
+   
+    func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
+        if userActivity.activityType == CSSearchableItemActionType {
+            NotificationCenter.default.post(name: NSNotification.Name.spotLightLineLookupNotification, object: nil, userInfo: userActivity.userInfo)
+            return true
+        }
+        return false
     }
 }
 
@@ -17,5 +27,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 private extension AppDelegate {
     func initCoreData() {
         _ = TFLBusStopStack.sharedDataStack
+    }
+    
+    func setupSpotLight() {
+        DispatchQueue.global().async {
+            let lineRouteList = TFLLineInfoRouteDirectory()
+            let provider = TFLCoreSpotLightDataProvider(with: lineRouteList)
+            provider.searchableItems { items in
+                CSSearchableIndex.default().indexSearchableItems(items) { error in
+                    if let _ = error {
+                        return
+                    }
+                }
+            }
+        }
     }
 }

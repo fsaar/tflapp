8cab9f5101c49aa1876e3bd6246938505249f55c
Frank Saar <frank.saar@gmail.com>
2019-02-13 22:22:45 +0000

    added splashscreen

--- tflapp/ViewController/TFLRootViewController/TFLRootViewController.swift
+++ tflapp/ViewController/TFLRootViewController/TFLRootViewController.swift
@@ -13,6 +13,7 @@ class TFLRootViewController: UIViewController {
         return radius
     }
     @IBOutlet weak var containerViewBottomConstraint : NSLayoutConstraint!
+    @IBOutlet weak var splashScreenContainerView : UIView!
     @IBOutlet weak var offlineView : UIView!
     fileprivate static let searchParameter  : (min:Double,initial:Double) = (100,500)
     fileprivate let networkBackgroundQueue = OperationQueue()
@@ -104,7 +105,9 @@ class TFLRootViewController: UIViewController {
     }
     private enum SegueIdentifier : String {
         case slideContainerController = "TFLSlideContainerControllerSegue"
+        case splashViewController = "TFLSplashscreenControllerSegue"
     }
+    
     fileprivate lazy var mapViewController : TFLMapViewController? = {
         guard let controller = self.storyboard?.instantiateViewController(withIdentifier: "TFLMapViewController") as? TFLMapViewController else {
             return nil
@@ -124,6 +127,7 @@ class TFLRootViewController: UIViewController {
         return view
     }()
     fileprivate var slideContainerController : TFLSlideContainerController?
+    fileprivate var splashScreenController : TFLSplashscreenController?
     private var foregroundNotificationHandler  : TFLNotificationObserver?
     private var backgroundNotificationHandler  : TFLNotificationObserver?
     @IBOutlet weak var errorContainerView : TFLErrorContainerView! = nil {
@@ -182,6 +186,11 @@ class TFLRootViewController: UIViewController {
         self.navigationController?.setNavigationBarHidden(true, animated: animated)
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        hideSplashscreenIfNeedBe()
+    }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let identifier = segue.identifier , let segueIdentifier = SegueIdentifier(rawValue: identifier) else {
             return
@@ -191,6 +200,10 @@ class TFLRootViewController: UIViewController {
             if let slideContainerController = segue.destination as? TFLSlideContainerController {
                 self.slideContainerController = slideContainerController
             }
+        case .splashViewController:
+            if let controller = segue.destination as? TFLSplashscreenController {
+                self.splashScreenController = controller
+            }
         }
     }
     var loadNearbyBusStopsCompletionBlocks : [CompletionBlock?] = []
@@ -199,6 +212,20 @@ class TFLRootViewController: UIViewController {
 // MARK: Private
 
 fileprivate extension TFLRootViewController {
+    func hideSplashscreenIfNeedBe() {
+        guard let _ = splashScreenContainerView.superview else {
+            return
+        }
+        UIView.animate(withDuration: 0.5, delay: 1.0, options: UIView.AnimationOptions.curveLinear, animations: {
+            self.splashScreenContainerView.alpha = 0
+        }) { _ in
+            self.removeController(self.splashScreenController)
+            self.splashScreenContainerView.removeFromSuperview()
+            self.splashScreenController = nil
+        }
+    }
+    
+    
     func showOfflineView(_ show : Bool = true) {
         self.containerViewBottomConstraint.constant = show ? self.offlineView.frame.size.height : 0
         UIView.animate(withDuration: 0.25) {

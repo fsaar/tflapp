403d5049f23f53033538d913a0600bb8df131433
Frank Saar <frank.saar@gmail.com>
2019-03-21 21:49:07 +0000

    refactored: extracted bounding rect calculation

--- tflapp/ViewController/TFLStationDetailController/TFLStationDetailMapBusRouteOverLay.swift
+++ tflapp/ViewController/TFLStationDetailController/TFLStationDetailMapBusRouteOverLay.swift
@@ -10,23 +10,31 @@ import UIKit
 import Foundation
 import MapKit
 
+extension Array where Element == CLLocationCoordinate2D {
+    var boundingRect : MKMapRect? {
+        let latSorted = self.sorted { $0.latitude < $1.latitude }
+        let longSorted = self.sorted { $0.longitude < $1.longitude }
+        guard let minLat = latSorted.first?.latitude, let maxLat = latSorted.last?.latitude,
+            let minLong = longSorted.first?.longitude, let maxLong = longSorted.last?.longitude else {
+                return nil
+        }
+        let p1 = MKMapPoint(CLLocationCoordinate2D(latitude: minLat, longitude: minLong))
+        let p2 = MKMapPoint(CLLocationCoordinate2D(latitude: maxLat, longitude: maxLong))
+        let boundingMapRect = MKMapRect(x: fmin(p1.x,p2.x), y: fmin(p1.y,p2.y), width: fabs(p1.x-p2.x), height: fabs(p1.y-p2.y))
+        return boundingMapRect
+    }
+}
+
 class TFLStationDetailMapBusRouteOverLay : NSObject, MKOverlay {
     var coordinate: CLLocationCoordinate2D = kCLLocationCoordinate2DInvalid
     var boundingMapRect: MKMapRect
     let model : TFLStationDetailMapViewModel
     init?(_ model : TFLStationDetailMapViewModel) {
-        let coords = model.stations.map { $0.coords }
-        let latSorted = coords.sorted { $0.latitude < $1.latitude }
-        let longSorted = coords.sorted { $0.longitude < $1.longitude }
-        guard let minLat = latSorted.first?.latitude, let maxLat = latSorted.last?.latitude,
-            let minLong = longSorted.first?.longitude, let maxLong = longSorted.last?.longitude else {
-                return nil
-        }
         self.model = model
-        let p1 = MKMapPoint(CLLocationCoordinate2D(latitude: minLat, longitude: minLong))
-        let p2 = MKMapPoint(CLLocationCoordinate2D(latitude: maxLat, longitude: maxLong))
-
-        boundingMapRect = MKMapRect(x: fmin(p1.x,p2.x), y: fmin(p1.y,p2.y), width: fabs(p1.x-p2.x), height: fabs(p1.y-p2.y))
-
+        guard let rect = model.coords.boundingRect else {
+            return nil
+        }
+        boundingMapRect = rect
+        super.init()
     }
 }

f83ef03539c460c0b76bf1764501fff6fe989199
Frank Saar <frank.saar@gmail.com>
2019-04-12 22:30:49 +0100

    cleanup

--- tflapp/Common/PolyLineDict/PolylineDict.swift
+++ tflapp/Common/PolyLineDict/PolylineDict.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 class PolylineDict {
-    let queue : OperationQueue = {
+    fileprivate let queue : OperationQueue = {
         let q = OperationQueue()
         q.maxConcurrentOperationCount = 1
         return q
@@ -39,9 +39,10 @@ class PolylineDict {
     }
     
     var innerDict : [String:String] = [:]
-    
     init() {
-        if let fileName = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).last?.appendingPathComponent("PolylineDict"),let dict = NSDictionary(contentsOf: fileName) {
+        
+        copyPolyLineDictIfNeedBe()
+        if let fileName = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).last?.appendingPathComponent("PolylineDict.plist"),let dict = NSDictionary(contentsOf: fileName) {
             print("Loaded PolylineDict: \(dict.allKeys.count) entries !")
             for (key,value) in dict {
                 if let (keyString,valueString) = (key,value) as? (String,String) {
@@ -52,11 +53,26 @@ class PolylineDict {
     }
     
     func save() {
-        guard let fileName = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).last?.appendingPathComponent("PolylineDict") else {
+        guard let fileName = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).last?.appendingPathComponent("PolylineDict.plist") else {
             return
         }
         let dict = NSDictionary(dictionary: innerDict)
         dict.write(to: fileName, atomically: true)
         print("saved! \(dict.allKeys.count) entries !")
     }    
+}
+
+fileprivate extension PolylineDict {
+    func copyPolyLineDictIfNeedBe() {
+        guard let sourceURL = Bundle.main.url(forResource: "PolylineDict", withExtension: "plist"),
+            let destinationURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).last?.appendingPathComponent("PolylineDict.plist") else {
+                return
+        }
+        if FileManager.default.fileExists(atPath: destinationURL.path) {
+            let date = "\(Date())".split(separator: " ").joined(separator: "")
+            let newPath = "\(destinationURL.path)\(date)"
+            try? FileManager.default.moveItem(atPath: destinationURL.path, toPath: newPath)
+        }
+        try? FileManager.default.copyItem(at: sourceURL, to: destinationURL)
+    }
 }

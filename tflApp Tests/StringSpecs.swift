3ad820c2f2ad9e369acce403e2f2fc767953ac23
Frank Saar <frank.saar@gmail.com>
2019-04-23 23:33:21 +0100

    enabled all tests

--- tflApp Tests/StringSpecs.swift
+++ tflApp Tests/StringSpecs.swift
@@ -7,22 +7,17 @@ class StringSpecs: QuickSpec {
     
     override func spec() {
         
-       
-        fit("should calculate the right SHA") {
+        it("should calculate the right SHA") {
             TFLHUD.show()
             let sha = "Test".sha256()
             expect(sha) == "Uy6qvZV0iA2/drm4zACDLCCm7BE9aCKZVQ16bg80XiU="
         }
         
-        fit("should calculate the right SHA with a different string") {
+        it("should calculate the right SHA with a different string") {
             TFLHUD.show()
             let sha = "TFLApp".sha256()
             expect(sha) == "6bXjBYFAczUtj+Siqt81eS79MCQW4VDVMgQWLX1oXqo="
         }
-        
-        
-        
-        
     }
             
 }

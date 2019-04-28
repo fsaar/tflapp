8caa6199abc1170e15c2d4b66afd4ccf864cba2c
Frank Saar <frank.saar@gmail.com>
2019-04-28 09:02:56 +0100

    delete old coredata persistentstore if needbe

--- tflapp/CoreData/TFLBusStopStack.swift
+++ tflapp/CoreData/TFLBusStopStack.swift
@@ -50,7 +50,48 @@ private let groupID =  "group.tflwidgetSharingData"
 
             return true
         }
+        
+        
+        func deleteOldPersistentStoreIfNeedBe()  {
+            
+            guard let dbFullFileName = dbFileName?.path, let path = dbFileName?.deletingPathExtension().path,let ext = dbFileName?.pathExtension,
+                let sourceURL = Bundle.main.url(forResource: path, withExtension: ext),
+                let destinationURL = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: groupID)?.appendingPathComponent(dbFullFileName)
+                else {
+                    return
+            }
+            if !FileManager.default.fileExists(atPath: destinationURL.path) {
+                return
+            }
 
+            do {
+                let dict : [String : Any] = [ NSMigratePersistentStoresAutomaticallyOption : true,
+                                              NSInferMappingModelAutomaticallyOption : true]
+                
+                let models = NSManagedObjectModel.mergedModel(from: nil)!
+                let coordinator = NSPersistentStoreCoordinator(managedObjectModel: models)
+                try coordinator.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: destinationURL, options: dict)
+                try coordinator.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: sourceURL, options: dict)
+                guard coordinator.persistentStores.count == 2 else {
+                    return
+                }
+                let store1 = coordinator.persistentStores[0]
+                let store2 = coordinator.persistentStores[1]
+                try store1.loadMetadata()
+                try store2.loadMetadata()
+                let metaData1 = store1.metadata as [String:Any]
+                let metaData2 = store2.metadata as [String:Any]
+                let version1 = metaData1[NSStoreUUIDKey] as? String ?? ""
+                let version2 = metaData2[NSStoreUUIDKey] as? String ?? ""
+                guard version1 != version2 else {
+                   return
+                }
+                try? FileManager.default.removeItem(at: destinationURL)
+            }
+            catch {
+                return
+            }
+        }
 
 
         func initCoreData(_ coordinator : NSPersistentStoreCoordinator) -> Bool {
@@ -73,13 +114,13 @@ private let groupID =  "group.tflwidgetSharingData"
             return true
         }
 
-
+        deleteOldPersistentStoreIfNeedBe()
         let models = NSManagedObjectModel.mergedModel(from: nil)!
         storeCoordinator = NSPersistentStoreCoordinator(managedObjectModel: models)
         if !initCoreData(storeCoordinator) {
    
             if cleanUpCoreData(storeCoordinator) {
-            
+                storeCoordinator = NSPersistentStoreCoordinator(managedObjectModel: models)
                 if !initCoreData(storeCoordinator) {
            
                     fatalError("Can't recover from Core Data initialisation")

1ce6299ada5deaf23ae620467a17cb87e37a0861
fsaar <frank.saar@gmail.com>
2023-07-18 19:13:37 +0100

    revised

--- tflapp/View/TFLNearbyBusStationListView/TFLBusPredictionListView.swift
+++ tflapp/View/TFLNearbyBusStationListView/TFLBusPredictionListView.swift
@@ -10,18 +10,16 @@ struct TFLBusPredictionListView : View {
     @Binding var predictionList : [TFLBusPrediction]
     @State var firstId : TFLBusPrediction.ID?
     var body : some View {
-        
         ScrollView(.horizontal,showsIndicators: false) {
             LazyVStack {
                 LazyHStack {
                     ForEach($predictionList) { prediction in
                         TFLBusPredictionView(prediction)
                             .transition(.opacity.animation(.linear(duration: 0.20)))
-                            .animation(.linear(duration: 0.4),value:predictionList)
+                        
                     }
-                }
+                } .animation(.linear(duration: 0.4),value:predictionList)
             }
-           
         }
         .scrollPosition(id: $firstId)
         .safeAreaPadding([.leading],10)

368374aac6fbce432346485b9301037666c66eb6
fsaar <frank.saar@gmail.com>
2023-07-18 19:25:08 +0100

    cleanup

--- tflapp/Common/Extensions/Collection+IndexPath.swift
+++
@@ -1,8 +0,0 @@
-import UIKit
-import Foundation
-
-extension Collection where Element == Int  {
-    func indexPaths(_ section : Int = 0) -> [IndexPath] {
-        return self.map{ IndexPath(item: $0, section: section) }
-    }
-}
